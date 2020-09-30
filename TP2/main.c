6c46bc93d956406ffa3bbe21e3f49108e19f3763
IgnacioAzzarita <68014520+IgnacioAzzarita@users.noreply.github.com>
2020-09-30 18:29:38 -0300

    Seguimo

--- TP2/main.c
+++ TP2/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAM 1000
+#define OCUPADO 0
+#define VACIO 1
+typedef struct
+{
+    int id;
+    char name[51];
+    char lastName[51];
+    float salary;
+    int sector;
+    int isEmpty;
+}Employee;
 int main()
 {
-    printf("Hello world!\n");
+    Employee arrayEmployee[TAM];
     return 0;
+}
+int initEmployees(Employee arrayEmployee[] ,int len)
+{
+    int i;
+    for(i=0;i<len;i++)
+    {
+        Employee empleado={0,"","",0.0,0,1};
+    }
+    return arrayEmployee;
 }
